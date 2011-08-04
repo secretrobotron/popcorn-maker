06b2ac31623167d33d220c7becb4a82d37649783
Christopher De Cairos <chris.decairos@gmail.com>
2011-08-04 15:34:47 -0400

    initial work on popcorn-maker +layout integration

--- js/popcorn-maker.js
+++ js/popcorn-maker.js
@@ -4,40 +4,31 @@
     
     var b  = new Butter();
 
-    b.eventeditor( { target: "butter-editor-div", defaultEditor: "lib/defaultEditor.html", editorWidth: "98%", editorHeight: "98%"  } );
+    b.eventeditor( { target: "popup-trackeditor", defaultEditor: "lib/defaultEditor.html", editorWidth: "98%", editorHeight: "98%"  } );
 
     b.previewer({
       layout: "layouts/default.html",
-      target: "previewer-iframe",
+      target: "main",
       media: "http://robothaus.org/bugs/video/brendan1.ogv",
       callback: function() {
-        b.buildPopcorn( "outerVideo", function() {
+        b.buildPopcorn( b.getCurrentMedia() , function() {
 
           var registry = b.getRegistry();
         } );
       }
     });
     
-    b.plugintray({ target: "butter-plug-in-div" });
+    b.plugintray({ target: "plugin-tray" });
     b.addPlugin( { type: "footnote" } );
     b.addPlugin( { type: "twitter" } );
     b.addPlugin( { type: "webpage" } );
     b.addPlugin( { type: "subtitle" } );
-    b.timeline({ target: "butter-timeline-div"});
-  
-    b.listen ( "trackeditstarted", function() {
-      overlay( "visible" );
-    });
-    b.listen ( "trackeditclosed", function() {
-      overlay( "hidden" );
-    });
+    b.addPlugin( { type: "googlenews" } );
+    
+    b.timeline({ target: "timeline-div"});
     
   }, false);
 
-  function overlay( style ) {
-    el = document.getElementById( "overlay-div" );
-    el.style.visibility = style;
-  }
 
 })();
 
