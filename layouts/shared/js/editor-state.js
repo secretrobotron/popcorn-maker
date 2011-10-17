9f572bdb486d7dd669e9b918b817e3f2a0db8037
Brian Chirls <brian@chirls.com>
2011-10-17 17:09:24 -0400

    1. EditorState now accepts default values for plugin parameters 2. Pop-Over editor has fade-out duration option

--- layouts/shared/js/editor-state.js
+++ layouts/shared/js/editor-state.js
@@ -281,7 +281,9 @@
 			options = message.trackEvent.popcornOptions;
 			for (n in that.fields) {
 				field = that.fields[n];
-				if (that.trackEvent[n] !== options[n]) {
+				if (options[n] === undefined && field.defaultValue !== undefined) {
+					that.trackEvent[n] = field.defaultValue;
+				} else if (that.trackEvent[n] !== options[n]) {
 					that.trackEvent[n] = options[n];
 					if (typeof field.callback === 'function') {
 						field.callback(field, that.trackEvent[n]);
