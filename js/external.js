170c9a305d56d2fbd5f92a49626fdf7560a180c3
David Seifried <david.c.seifried@gmail.com>
2011-12-02 17:36:01 -0500

    FIXED QTIP ERROR FOREVER, HOPEFULLY, PROBABLY NOT

--- js/external.js
+++ js/external.js
@@ -1,20 +1,14 @@
 (function() {
 
-  require( [ "lib/jquery.min.js" ], function() {
-
-    require( [ "lib/jquery-ui.js" ], function() {
+  require( [ "lib/jquery.min.js", "lib/jquery.qtip.pack.js" ], function() {
 
       require([
+        "lib/jquery-ui.js",
         "lib/modernizr-1.5.min.js",
         "lib/jquery.infieldlabel.min.js",
         "lib/jquery.tinyscrollbar.min.js",
         "lib/slides.min.jquery.js",
-        "lib/jquery_002.js",
-        "lib/jquery.qtip.pack.js",
+        "lib/jquery_002.js"
       ], function() {} );
-
-    });
-
   });
-
 })();
