b8a8fb8c78f387a93de8526b88e44202c930849f
David Seifried <david.c.seifried@gmail.com>
2011-12-02 15:40:18 -0500

    Removed leftover reference to the slide plugin

--- js/external.js
+++ js/external.js
@@ -6,7 +6,6 @@
         "lib/modernizr-1.5.min.js",
         "lib/jquery.infieldlabel.min.js",
         "lib/jquery.tinyscrollbar.min.js",
-        "lib/slides.min.jquery.js",
         "lib/jquery_002.js",
         "lib/jquery-ui.js",
         "lib/jquery.qtip.pack.js",
