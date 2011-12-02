991ffcee8ccc208caf1d854a6e220526abe5da34
Bobby Richter <secretrobotron@gmail.com>
2011-12-02 17:54:14 -0500

    Used a define in external lib loader in PM. Need order!

--- js/external.js
+++ js/external.js
@@ -1,14 +1,21 @@
 (function() {
 
-  define( [ "lib/jquery.min.js", "lib/jquery.qtip.pack.js" ], function() {
+  define( [ "lib/jquery.min.js" ], function() {
 
-      define([
-        "lib/jquery-ui.js",
-        "lib/modernizr-1.5.min.js",
+    require( [
+        "lib/jquery.qtip.pack.js",
         "lib/jquery.infieldlabel.min.js",
-        "lib/jquery.tinyscrollbar.min.js",
         "lib/slides.min.jquery.js",
-        "lib/jquery_002.js"
-      ], function() {} );
+        "lib/jquery_002.js",
+        "lib/modernizr-1.5.min.js",
+        "lib/jquery.tinyscrollbar.min.js"
+      ], function() {
+
+        require([
+          "lib/jquery-ui.js"
+        ], function() {
+        } );
+
+    });
   });
 })();
